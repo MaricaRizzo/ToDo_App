7ff5f5fde3b3d75b9b91304d5255f510eef2993e
Marica <maricarizzoo@gmail.com>
2024-11-12 18:32:53 +0100

    Added prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-       <p>Hello World!</p>
+        <p>Hello World!</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
